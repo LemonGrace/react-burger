f65bd6044e383db8737b8d2a119c3e1301ad5b74
Veronica <vdgrechko@yandex.ru>
2022-07-25 20:14:46 +0300

    fix for gp

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import { render } from "react-dom";
 import App from './components/app/app';
 import { Provider } from 'react-redux';
